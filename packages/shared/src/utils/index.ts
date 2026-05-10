31658ce734269ea85c468b4d92c1f49f5341e5d0
Aron Greenspan <aron.greenspan@inixiative.com>
2026-05-09 23:41:18 -0300

    chore(shared): export isUuidV7 from utils barrel

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,4 +1,5 @@
 export { cn } from './cn';
 export { ConcurrencyType, concurrencyLimits, getConcurrency } from './concurrency';
 export { type Environment, isLocal, isPR, isProd, isStaging, isTest } from './env';
+export { isUuidV7 } from './isUuidV7';
 export { resolveAll } from './resolveAll';
